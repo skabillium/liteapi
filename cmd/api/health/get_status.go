85afd2fce2bf5bb754a5e1653c9ef2a9305c9c28
Bill Kyriazopoulos <skabillium@gmail.com>
2024-07-03 10:53:15 +0300

    Make status response a local variable to avoid extra allocations

--- cmd/api/health/get_status.go
+++ cmd/api/health/get_status.go
@@ -6,12 +6,13 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// Standard status up response
+var statusUp = gin.H{"message": "up"}
+
 // @Tags Health
 // @Description Check the status of the server
 // @Success 200
 // @Router /status [get]
 func GetStatusHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "up",
-	})
+	c.JSON(http.StatusOK, statusUp)
 }
